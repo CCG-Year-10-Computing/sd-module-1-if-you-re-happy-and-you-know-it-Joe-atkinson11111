c36d029d7d15095c69b1cb94057bab954ed9e62a
Noa.Watson <Noa.Watson@S26-5CG538182P>
2026-02-11 12:57:21 +1100

    added first question

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
-﻿// Program Name: 
-// Created on: 
+﻿// Program Name: ed buy me car
+// Created on: today
 // Author: Noah
 
+string name;
 
+Console.WriteLine("should ed buy me a nissan 350z?");
+string response = Console.ReadLine();
+
+if (response == "yes")
+{
+    Console.WriteLine("thats good");
+}
+else (response == "no");
+{
+    Console.WriteLine("blasphemy");
+}
