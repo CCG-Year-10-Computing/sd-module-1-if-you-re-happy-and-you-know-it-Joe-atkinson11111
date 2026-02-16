748735adabb6c4a6933fafc16600e6a2a98a2483
Noa.Watson <Noa.Watson@S26-5CG538182P>
2026-02-17 09:48:38 +1100

    finish project

--- Program.cs
+++ Program.cs
@@ -1,17 +1,53 @@
-﻿// Program Name: ed buy me car
-// Created on: today
-// Author: Noah
+﻿// Program Name:
+
+// Created on:
+
+// Author:
+
+using System.ComponentModel.DataAnnotations;
+
+using System.Runtime.InteropServices;
+
+
+
+int rating;
 
 string name;
 
-Console.WriteLine("should ed buy me a nissan 350z?");
-string response = Console.ReadLine();
+string car;
 
-if (response == "yes")
+string edbuycar;
+
+string money;
+
+Console.WriteLine("what is your name?");
+name = Console.ReadLine();
+Console.WriteLine("what is your favourite car");
+car = Console.ReadLine();
+Console.WriteLine("should ed buy you a car?");
+edbuycar = Console.ReadLine();
+Console.WriteLine("how much is ed going to spend on your car");
+money = Console.ReadLine();
+
+
+
+Console.WriteLine($"{name} wants a {car}, smiling as Ed is about to you a {car} " +
+
+    $" . {name} cant believe Ed will spend {money} on a car for you. As the {car} is going to be realy cool, the {car} is going " +
+
+    $" to need lots of work . {name} is happy because ed will pay for it.");
+
+Thread.Sleep(100);
+
+Console.WriteLine("rate this program a 10!");
+
+rating = Console.Read();
+
+if (rating > 9)
 {
-    Console.WriteLine("thats good");
+    Console.WriteLine("good boy!");
 }
-else (response == "no");
+else
 {
-    Console.WriteLine("blasphemy");
+    Console.WriteLine("never message me again.");
 }
